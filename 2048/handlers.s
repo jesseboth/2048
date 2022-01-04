d52c0c7af8d90ee7a8de3401e827a16eeadc2f2d
jesseboth <jessebot@buffalo.edu>
2022-01-04 15:11:53 -0500

    Adjusted to start game when enter is pressed

--- 2048/handlers.s
+++ 2048/handlers.s
@@ -1,18 +1,20 @@
     .data
 time_string:    .string "00:00:00", 0
 time_int:       .int 0
-prev_mov:       .char 0
 
-game_active     .char 1     ; 1 when active, 0 by default
+game_active     .char 0     ; 1 when active, 0 by default
     .text
     .global Timer_Handler
     .global Switch_Handler
+    .global Keypad_Handler
     .global UART0_Handler
 
     .global shift_right_wrapper
     .global shift_left_wrapper
     .global shift_down_wrapper
     .global shift_up_wrapper
+
+    .global start_game
 
     ;* lib
     .export time_to_string
@@ -24,10 +26,16 @@ game_active     .char 1     ; 1 when active, 0 by default
 
     ;* library
     .global read_character
+    .global read_from_push_btn
+    .global output_character
+
+    ;* keypad
+    .export keypad_clear_interrupt
+    .export keypad_read
+    .export keypad_wait
 
 ptr_to_time_string: .word time_string
 ptr_to_time_int:    .word time_int
-ptr_to_prev_mov:    .word prev_mov
 
 ptr_to_game_active: .word game_active
 
@@ -35,9 +43,10 @@ W:  .equ 0x77
 A:  .equ 0x61
 S:  .equ 0x73
 D:  .equ 0x64
+ENTER: .equ 0xD
 
 ;************************Handlers************************
-;* clear timer interupt and does "stuff"
+;* clear timer interrupt and does "stuff"
 ;* input  -
 ;* output -
 Timer_Handler:
@@ -74,17 +83,49 @@ _exit_timer_handler
     LDMFD sp!, {r0-r12,lr}
     BX lr
 
-;* clear switch interupt and does "stuff"
+;* clear switch interrupt and does "stuff"
 ;* input  -
 ;* output -
 Switch_Handler:
     STMFD SP!,{r0-r12,lr}
 
+    mov r1, #0x5000
+    movt r1, #0x4002
+    ldrb r2, [r1,#0x41C]
+    orr r3, r2, #0x10
+    strb r3, [r1,#0x41C]
+
+    bl read_from_push_btn
 
     LDMFD sp!, {r0-r12,lr}
     BX lr
 
-;* clear UART interupt and does "stuff"
+;* clear switch interrupt and does operation
+;* input  -
+;* output -
+Keypad_Handler:
+    STMFD SP!,{r0-r12,lr}
+    bl keypad_clear_interrupt
+
+    bl keypad_wait
+    cmp r0, #0
+    beq _exit_Keypad_Handler
+
+    bl keypad_read
+    mov r4, r0
+
+    ldr r0, ptr_to_game_active
+    ldrb r0, [r0]
+    cmp r0, #0
+    beq _exit_Keypad_Handler
+
+    mov r0, r4
+    bl board_move
+_exit_Keypad_Handler:
+    LDMFD sp!, {r0-r12,lr}
+    BX lr
+
+;* clear UART interrupt and does "stuff"
 ;* input  -
 ;* output -
 UART0_Handler:
@@ -96,46 +137,63 @@ UART0_Handler:
     mov r2, #1
     bfi r1, r2, #4, #1  ; place 1 to clear interrupt
     strb r1, [r0,#0x044]
-
-    ldr r0, ptr_to_game_active
-    ldrb r0, [r0]
-    cmp r0, #0
-    beq _exit_UART_Handler
-
-    bl read_character
-    mov r4, r0
-    ldr r5, ptr_to_prev_mov
-    ldrb r1, [r5]
     
-    cmp r4, #W
-    beq _up_pressed
+    bl read_character
 
-    cmp r4, #A
-    beq _left_pressed
+    ldr r4, ptr_to_game_active
+    ldrb r4, [r4]
+    cmp r4, #0
+    bne _board_mover
 
-    cmp r4, #S
-    beq _down_pressed
+    cmp r0, #ENTER
+    bne _exit_UART_Handler
 
-    cmp r4, #D
-    beq _right_pressed
+    mov r0, #1
+    ldr r1, ptr_to_game_active
+    strb r0, [r1]
+    bl start_game
 
     b _exit_UART_Handler
-
-_up_pressed:
-    bl shift_up_wrapper
-    b _store_mov
-_left_pressed:
-    bl shift_left_wrapper
-    b _store_mov
-_down_pressed:
-    bl shift_down_wrapper
-    b _store_mov
-_right_pressed:
-    bl shift_right_wrapper
-_store_mov:
-    strb r4, [r5]
+_board_mover:
+    bl board_move
 _exit_UART_Handler:
     LDMFD sp!, {r0-r12,lr}
     BX lr
+
+;* moves the board from interrupt
+;* input - r0 (WASD)
+board_move:
+    STMFD SP!,{r4-r12,lr} 
+    
+    cmp r0, #W
+    beq _up_pressed
+
+    cmp r0, #A
+    beq _left_pressed
+
+    cmp r0, #S
+    beq _down_pressed
+
+    cmp r0, #D
+    beq _right_pressed
+
+    b _exit_board_move
+
+_up_pressed:
+    bl shift_up_wrapper
+    b _exit_board_move
+_left_pressed:
+    bl shift_left_wrapper
+    b _exit_board_move
+_down_pressed:
+    bl shift_down_wrapper
+    b _exit_board_move
+_right_pressed:
+    bl shift_right_wrapper
+
+_exit_board_move:
+    LDMFD sp!, {r4-r12,lr}       
+    MOV pc, lr
+
 
 .end
