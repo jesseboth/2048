c872e585b5aa2a7fd9d87e0470a0154a02823e76
jesseboth <jessebot@buffalo.edu>
2022-01-04 15:11:35 -0500

    outputs title screen and routine to start game

--- 2048/game.s
+++ 2048/game.s
@@ -1,25 +1,33 @@
     .data
-test: .string "test", 0xA, 0xD, 0
     
     .text
     .global start
+    .global start_game
 
     .global output_boarder
     .global place_square
     .global draw_board 
     .global random_tile
     .global clear_board 
-    .global shift_right_wrapper
-    .global shift_left_wrapper
-    .global shift_down_wrapper
-    .global shift_up_wrapper
+    .global make_title
 
     .global output_string
     .global init
 
-ptr_to_test: .word test
-
 start:
+    STMFD SP!,{r0-r12,lr}
+
+    bl make_title
+
+_again:
+    wfi
+    b _again
+
+    LDMFD sp!, {r0-r12,lr}
+    MOV pc, lr
+
+;* starts the game
+start_game:
     STMFD SP!,{r0-r12,lr}
 
     bl output_boarder
@@ -27,10 +35,6 @@ start:
     bl random_tile
 
     bl draw_board
-
-_again:
-    wfi
-    b _again
 
     LDMFD sp!, {r0-r12,lr}
     MOV pc, lr
