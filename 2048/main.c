a83a884a8d1542616dd63d5aa56068bd04bd85a4
jesseboth <jessebot@buffalo.edu>
2022-01-03 21:09:40 -0500

    keypad adjustments

--- 2048/main.c
+++ 2048/main.c
@@ -1,26 +1,21 @@
-
-
-/**
- * main.c
- */
 #include "LCD.h"
-extern void output_string(char *str);
+#include "keypad.h"
+
 extern void start(void);
 extern void init(void);
 
 int main(void) {
 	init();
+	
 	LCD_init();
-
-
 	LCD_setCursor(0, 0);
 	LCD_print((char *)"Time: 00:00");
 	LCD_setCursor(0, 1);
 	LCD_print((char *)"Score: 0");
 
-	start();
+	 keypad_init();
 
+	 start();
 
-	// output_string((char *)"DONE\n\r");
 	return 0;
 }
